8c820a12df9cab843b3417ec4975758fbf6fa66a
Lalafang33 <lalafang33@gmail.com>
2023-01-19 11:26:25 -0800

    added new state for the form and url

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,47 @@
-import React from "react"
+import {React, useState} from "react"
 import "./Form.css"
-import meme from "../memesData"
+import memes from "../memesData"
 
 export default function Form() {
+  // const [memeImage, setMemeImage]= useState("")
+
+  const [meme, setMeme] = useState({
+    topText: "", 
+    bottomText: "", 
+    randomImage:"http://i.imgflip.com/1bij.jpg"
+  })
+
+    // const arr = meme.data.memes;
+    // const randomMeme = arr[Math.floor(Math.random() * arr.length)]
+    // const memeUrl = randomMeme.url
+
+  const [allMemeImages, setAllMemeImages] = useState(memes)
+
 
   function clicked() {
-
-    const arr = meme.data.memes;
+    const arr = allMemeImages.data.memes;
     const randomMeme = arr[Math.floor(Math.random() * arr.length)]
-   
-   console.log(randomMeme.id)
+    const memeUrl = randomMeme.url
+    // console.log(randomMeme.url)
 
+    setMeme(prevState => ({
+        ...prevState, 
+        randomImage: memeUrl, 
+      })
+    )
   }
 
   return(
     <div className="form-container">
-        <form className="form-text">
+      <div className="form-text">
           <input type="text" placeholder="shut up and "></input>
           <input type="text" placeholder="take my money"></input>
-        </form>
-      <button onClick={clicked} className="form-button">Get a New Meme Image 🖼 </button>
+      </div>
+      <button 
+        onClick={clicked} 
+        className="form-button">Get a New Meme Image 🖼 </button>
+
+      <img src={meme.randomImage} alt="randomly generate meme" />
 
     </div>
   )
