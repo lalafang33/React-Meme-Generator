c8704a717b97ce2a904426710b014ad25f79901a
Lalafang33 <lalafang33@gmail.com>
2023-01-23 20:34:20 -0800

    changed hard coded data page to use api call

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import {React, useState} from "react"
+import {React, useState, useEffect} from "react"
 import "./Form.css"
 import memes from "../memesData"
 
@@ -15,13 +15,13 @@ export default function Form() {
     // const randomMeme = arr[Math.floor(Math.random() * arr.length)]
     // const memeUrl = randomMeme.url
 
-  const [allMemeImages, setAllMemeImages] = useState(memes)
+  const [allMemeImages, setAllMemeImages] = useState([])
 
 
   function clicked() {
-    const arr = allMemeImages.data.memes;
-    const randomMeme = arr[Math.floor(Math.random() * arr.length)]
-    const memeUrl = randomMeme.url
+    // const arr = allMemeImages.data.memes;
+    const randomNumber = [Math.floor(Math.random() * allMemeImages.length)]
+    const memeUrl = allMemeImages[randomNumber].url
     // console.log(randomMeme.url)
 
     setMeme(prevState => ({
@@ -30,6 +30,19 @@ export default function Form() {
       })
     )
   } 
+
+  useEffect(function() {
+    fetch(`https://api.imgflip.com/get_memes`)
+    .then(res => res.json())
+    .then(data => setAllMemeImages(data.data.memes))
+  },[])
+
+  // function clicked() {
+  //   setMeme(prevState => ({
+  //     ...prevState, 
+  //     randomImage:
+  //   }))
+  // }
 
   function handleChange(event) {
     const {name, value} = event.target 
